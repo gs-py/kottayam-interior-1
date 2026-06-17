6f0fafadf49277f9de4a4b733298a2cfd51f3b22
Gladwin-32 <santhosh@lyfskills.co>
2026-06-17 10:20:52 +0530

    updated for tanstack qeery

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
 import { Toaster } from '@/components/ui/toaster'
 import './index.css'
 
-const queryClient = new QueryClient()
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-          <Toaster />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
 )
